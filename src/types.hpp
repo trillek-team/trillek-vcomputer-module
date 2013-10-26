e28e6eef5a8a528d360b177ca49dfec296f1c6a6
Luis Panadero Guardeño <luis.panadero@gmail.com>
2013-10-26 17:18:13 +0200

    Portable type.hpp

--- src/types.hpp
+++ src/types.hpp
@@ -9,9 +9,35 @@
 
 #include "endian.h"
 
+#if (! defined(_MSC_VER) || (_MSC_VER >= 1700))
 #include <cstdint>
+#endif
 
 namespace vm {
+
+// Visual Studio 6 and Embedded Visual C++ 4 doesn't
+// realize that, e.g. char has the same size as __int8
+// so we give up on __intX for them.
+#if (defined(_MSC_VER) && (_MSC_VER < 1700))
+    #if (_MSC_VER < 1300)
+        typedef signed char       int8_t;
+        typedef signed short      int16_t;
+        typedef signed int        int32_t;
+        typedef unsigned char     uint8_t;
+        typedef unsigned short    uint16_t;
+        typedef unsigned int      uint32_t;
+    #else
+        typedef signed __int8     int8_t;
+        typedef signed __int16    int16_t;
+        typedef signed __int32    int32_t;
+        typedef unsigned __int8   uint8_t;
+        typedef unsigned __int16  uint16_t;
+        typedef unsigned __int32  uint32_t;
+    #endif
+    typedef signed __int64       int64_t;
+    typedef unsigned __int64     uint64_t;
+#endif
+
 
 typedef uint64_t qword_t;   /// Quad WORD
 typedef uint32_t dword_t;   /// Double WORD
