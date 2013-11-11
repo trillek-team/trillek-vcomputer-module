772d9821ca2d558aa303ecdfc4cfd68c1ba976b1
Luis Panadero Guardeño <luis.panadero@gmail.com>
2013-11-11 18:18:42 +0100

    Added callback to CDA to allow upload data to GPU or send vua network without needing a intermediate buffer

--- include/CDA.hpp
+++ include/CDA.hpp
@@ -28,27 +28,34 @@ unsigned const VRAM_SIZE = 0x4400;      /// VRAM size
 dword_t  const SETUP_OFFSET = 0xCC00;   /// SETUP register offset
 
 dword_t INT_MSG[] = {                   /// Interrupt message value
-		0x0000005A, 
-		0x0000105A, 
-		0x0000205A, 
-		0x0000305A};
+    0x0000005A, 
+    0x0000105A, 
+    0x0000205A, 
+    0x0000305A};
 
 dword_t BASE_ADDR[] = {                 /// VRAM base address
-		0xFF0A0000, 
-		0xFF0B0000, 
-		0xFF0C0000, 
-		0xFF0D0000};
+    0xFF0A0000, 
+    0xFF0B0000, 
+    0xFF0C0000, 
+    0xFF0D0000};
 
+class CDA;
+
+typedef void (*VSync_CBack)(CDA*, const byte_t*); /// Callback for virtual VSync (to upload video data to GPU or send it by network)
+
+/**
+ * Color Display Adapter device
+ */
 class CDA : public IDevice {
 public:
 
-CDA() : count(0), videomode(0), textmode(true), blink(false), userfont(false), e_vsync(false), vram(this), setupr(this), buffer(NULL) {
+CDA() : count(0), videomode(0), textmode(true), blink(false), userfont(false), e_vsync(false), vram(this), setupr(this), buffer(nullptr), vsync_cb(nullptr) {
     buffer = new byte_t[VRAM_SIZE]();
 }
 
 virtual ~CDA() {
-    if (buffer != NULL)
-	    delete[] buffer;
+    if (buffer != nullptr)
+      delete[] buffer;
 }
 
 byte_t DevClass() const     {return 0x0E;}   // Graphics device
@@ -57,218 +64,231 @@ word_t DevId() const        {return 0x0001;} // CDA standard
 word_t DevVer() const       {return 0x0000;} // Ver 0 -> CDA base standard
 
 virtual void Tick (cpu::RC3200& cpu, unsigned n=1) {
-    count += n;
-	if (count >= (cpu.Clock()/VSYNC)) { // V-Sync Event
-	    count -= cpu.Clock()/VSYNC;
-				
-		    if (e_vsync)
-			    cpu.ThrowInterrupt(INT_MSG[this->Jmp1() &3]);
-	}
+  // TODO Make VSync async respect CPU clock speed and use realtime 25 Hz
+  count += n;
+  if (count >= (cpu.Clock()/VSYNC)) { // V-Sync Event
+    count -= cpu.Clock()/VSYNC;
+       
+    // try to call to the callback
+    if (vsync_cb != nullptr)
+      vsync_cb (this, buffer);
+
+    if (e_vsync)
+      cpu.ThrowInterrupt(INT_MSG[this->Jmp1() &3]);
+  }
 }
 
 virtual std::vector<ram::AHandler*> MemoryBlocks() const { 
-    auto handlers = IDevice::MemoryBlocks(); 
-    handlers.push_back((ram::AHandler*)&vram);
-    handlers.push_back((ram::AHandler*)&setupr);
+  auto handlers = IDevice::MemoryBlocks(); 
+  handlers.push_back((ram::AHandler*)&vram);
+  handlers.push_back((ram::AHandler*)&setupr);
 
-    return handlers;
+  return handlers;
 }
 
 /**
  * Video RAM buffer
  */
 const byte_t* VRAM() const {
-		return buffer;
+  return buffer;
 }
 
 /**
  * Actual videomode
  */
 unsigned VideoMode() const {
-		return videomode;
+  return videomode;
 }
 
 /**
  * Is using a Text Videomode ?
  */
 bool isTextMode() const {
-		return textmode;
+  return textmode;
 }
 
 /**
  * Bright background color attribute is blink, for Text videomode ?
  */
 bool isBlinkAttr() const {
-		return blink;
+  return blink;
 }
 
 /**
  * Is using User defined font in a Text videomode ?
  */
 bool isUserFont() const {
-		return userfont;
+  return userfont;
 }
 
 /**
- * Generates/Updates a RGBA texture (4 byte per pixel) of the screen state
+ * Sets the VSync callback function
+ * Set it to allow do stuff every time that a vsync event happens, like upload 
+ * vram to GPU or send VRAM to a remote client
+ * @param new_cb Callback function. Set to nullptr to disable it.
+ */
+void VSyncCallBack (VSync_CBack new_cb) {
+  vsync_cb = new_cb;
+}
+
+/**
+ * Generates/Updates a RGB texture (3 byte per pixel) of the screen state
  * @param texture Ptr. to the texture. Must have a size enought to containt a 640x200 RGBA texture.
  */
-void RGBATexture (dword_t* texture) const {
-		assert(texture != NULL);
+void RGBTexture (dword_t* texture) const {
+  assert(texture != NULL);
 
-		dword_t fg, bg;
-		unsigned addr;
+  dword_t fg, bg;
+  unsigned addr;
 
-		if (textmode) {
-				// Text mode
-				if (videomode == 0) {
-						// 40x25 -> 320x200
+  if (textmode) {
+    // Text mode
+    if (videomode == 0) {
+        // 40x25 -> 320x200
 
-				} else if (videomode == 1) {
-						// 80x25 -> 640x200
+    } else if (videomode == 1) {
+        // 80x25 -> 640x200
 
-				} // else -> Unknow videomode. Not supported
-		} else {
-				// Graphics mode
-				byte_t attr, pixels, pix;
-				if (videomode == 0) {
-						// 320x200
-						// (X,Y) = ((X % 320)/8) + (320 * Y / 8)     pixel bit (X) = X%8
-						// attribute (X,Y) = 8000 + ((X % 320)/8) + (320 * Y / 8)/8
-						
-						for (unsigned y = 0; y < 200; y++ ) {
-								for (unsigned x = 0; x < 320; x++ ) {
-										pix = x % 8;
+    } // else -> Unknow videomode. Not supported
+  } else {
+    // Graphics mode
+    byte_t attr, pixels, pix;
+    if (videomode == 0) {
+      // 320x200
+      // (X,Y) = ((X % 320)/8) + (320 * Y / 8)     pixel bit (X) = X%8
+      // attribute (X,Y) = 8000 + ((X % 320)/8) + (320 * Y / 8)/8
+      
+      for (unsigned y = 0; y < 200; y++ ) {
+        for (unsigned x = 0; x < 320; x++ ) {
+          pix = x % 8;
 
-										if (x % 8 == 0) { // Update color only when we fin the next 8x8 cell
-												// 1 - Get Fg and Bg colors
-												addr = 8000 + (x / 8) + (320/8) * y /8;
-												attr = buffer[addr];
-												fg = attr & 7;
-												if (!blink) {
-														fg = (attr & 8) ? fg +9 : fg;
-												} // TODO Blink
-												bg = attr & 0xF;
+          if (x % 8 == 0) { // Update color only when we fin the next 8x8 cell
+            // 1 - Get Fg and Bg colors
+            addr = 8000 + (x / 8) + (320/8) * y /8;
+            attr = buffer[addr];
+            fg = attr & 7;
+            if (!blink) {
+                fg = (attr & 8) ? fg +9 : fg;
+            } // TODO Blink
+            bg = attr & 0xF;
 
-												// 2 - Get the 8 pixel row from VRAM
-												addr = x / 8 + (320/8) * y;
-												pixels = buffer[addr];
-										}
+            // 2 - Get the 8 pixel row from VRAM
+            addr = x / 8 + (320/8) * y;
+            pixels = buffer[addr];
+          }
 
-										// 3 - Put the pixel in the texture
-										if (pixels & (1 << pix)) {  // Active   -> Foreground
-												texture[x + y*320] = fg;
-										} else {                    // Unactive -> Background
-												texture[x + y*320] = bg;
-										}
-								}
-						}
+          // 3 - Put the pixel in the texture
+          if (pixels & (1 << pix)) {  // Active   -> Foreground
+            texture[x + y*320] = fg;
+          } else {                    // Unactive -> Background
+            texture[x + y*320] = bg;
+          }
+        }
+      }
 
-				} else if (videomode == 1) {
-						// 640x200
-						// (X,Y) = ((X % 640)/8) + (640 * Y / 8) pixel bit (X) = X%8
-						// attribute (X,Y) = 16000 + ((X % 640)/8) + (640 * Y / 8)/8
+    } else if (videomode == 1) {
+      // 640x200
+      // (X,Y) = ((X % 640)/8) + (640 * Y / 8) pixel bit (X) = X%8
+      // attribute (X,Y) = 16000 + ((X % 640)/8) + (640 * Y / 8)/8
 
-						for (unsigned y = 0; y < 200; y++ ) {
-								for (unsigned x = 0; x < 640; x++ ) {
-										pix = x % 8;
+      for (unsigned y = 0; y < 200; y++ ) {
+        for (unsigned x = 0; x < 640; x++ ) {
+          pix = x % 8;
 
-										if (x % 8 == 0) { // Update color only when we fin the next 8x8 cell
-												// 1 - Get Fg and Bg colors
-												addr = 16000 + (x / 8) + (640/8) * y /8;
-												attr = buffer[addr];
-												fg = attr & 7;
-												if (!blink) {
-														fg = (attr & 8) ? fg +9 : fg;
-												} // TODO Blink
-												bg = attr & 0xF;
+          if (x % 8 == 0) { // Update color only when we fin the next 8x8 cell
+            // 1 - Get Fg and Bg colors
+            addr = 16000 + (x / 8) + (640/8) * y /8;
+            attr = buffer[addr];
+            fg = attr & 7;
+            if (!blink) {
+                fg = (attr & 8) ? fg +9 : fg;
+            } // TODO Blink
+            bg = attr & 0xF;
 
-												// 2 - Get the 8 pixel row from VRAM
-												addr = x / 8 + (640/8) * y;
-												pixels = buffer[addr];
-										}
+            // 2 - Get the 8 pixel row from VRAM
+            addr = x / 8 + (640/8) * y;
+            pixels = buffer[addr];
+          }
 
-										// 3 - Put the pixel in the texture
-										if (pixels & (1 << pix)) {  // Active   -> Foreground
-												texture[x + y*640] = fg;
-										} else {                    // Unactive -> Background
-												texture[x + y*640] = bg;
-										}
-								}
-						}
+          // 3 - Put the pixel in the texture
+          if (pixels & (1 << pix)) {  // Active   -> Foreground
+            texture[x + y*640] = fg;
+          } else {                    // Unactive -> Background
+            texture[x + y*640] = bg;
+          }
+        }
+      }
 
-				} // else -> Unknow videomode. Not supported
-		}
+    } // else -> Unknow videomode. Not supported
+  }
 }
 
 const static dword_t pallete[]; /// Fixed Color pallette
 
 protected:
 
-/**
- * Address Handler that manages the VideoRAM
- */
-class VideoRAM : public ram::AHandler {
-public:
-		VideoRAM (CDA* cda) {
-				this->cda = cda;
-				this->begin = BASE_ADDR[cda->Jmp1() &3];
-				this->size = VRAM_SIZE;
-		}
+  /**
+   * Address Handler that manages the VideoRAM
+   */
+  class VideoRAM : public ram::AHandler {
+  public:
+    VideoRAM (CDA* cda) {
+      this->cda = cda;
+      this->begin = BASE_ADDR[cda->Jmp1() &3];
+      this->size = VRAM_SIZE;
+    }
 
-		virtual ~VideoRAM() {
-		}
+    virtual ~VideoRAM() {
+    }
 
-		byte_t RB (dword_t addr) {
-				addr -= this->begin;
-				assert(addr < this->size);
-				assert(addr >= 0);
+    byte_t RB (dword_t addr) {
+      addr -= this->begin;
+      assert(addr < this->size);
+      assert(addr >= 0);
 
-				return cda->buffer[addr];
-		}
+      return cda->buffer[addr];
+    }
 
-		void WB (dword_t addr, byte_t val) {
-				//std::printf("CDA -> ADDR: %08Xh VAL : 0x%02X\n", addr, val);
-				addr -= this->begin;
-				assert(addr < this->size);
-				assert(addr >= 0);
-				
-				cda->buffer[addr] = val;
-		}
+    void WB (dword_t addr, byte_t val) {
+      addr -= this->begin;
+      assert(addr < this->size);
+      assert(addr >= 0);
+      
+      cda->buffer[addr] = val;
+    }
 
 
-		CDA* cda; // Self-reference
-};
+    CDA* cda; // Self-reference
+  };
 
-/**
- * Address block that manages the SETUP register
- */
-class SETUPreg : public ram::AHandler {
-public:
-		SETUPreg (CDA* cda) {
-				this->begin = BASE_ADDR[cda->Jmp1() &3] + SETUP_OFFSET;
-				this->size = 1;
-				this->cda = cda;
-		}
+  /**
+   * Address block that manages the SETUP register
+   */
+  class SETUPreg : public ram::AHandler {
+  public:
+    SETUPreg (CDA* cda) {
+      this->begin = BASE_ADDR[cda->Jmp1() &3] + SETUP_OFFSET;
+      this->size = 1;
+      this->cda = cda;
+    }
 
-		byte_t RB (dword_t addr) {
-				return reg;
-		}
+    byte_t RB (dword_t addr) {
+      return reg;
+    }
 
-		void WB (dword_t addr, byte_t val) {
-				reg = val;
-				cda->videomode = val & 7; 
-				cda->textmode = (val & 8) == 0;
-				cda->blink = (val & 0x20 ) != 0;
-				cda->userfont = (val & 0x40 ) != 0;
-				cda->e_vsync = (val & 0x80 ) != 0;
-				
-		}
+    void WB (dword_t addr, byte_t val) {
+      reg = val;
+      cda->videomode = val & 7; 
+      cda->textmode = (val & 8) == 0;
+      cda->blink = (val & 0x20 ) != 0;
+      cda->userfont = (val & 0x40 ) != 0;
+      cda->e_vsync = (val & 0x80 ) != 0;
+    }
 
-		byte_t reg;
-		CDA* cda; // Self-reference
+    byte_t reg;
+    CDA* cda; // Self-reference
 
-};
+  };
 
 unsigned count;         /// Cycle counter
 
@@ -282,6 +302,8 @@ CDA::VideoRAM vram;
 CDA::SETUPreg setupr;
 
 byte_t* buffer;        /// Visible video ram buffer for grpahics representation of screen
+
+VSync_CBack vsync_cb;
 
 };
 
--- src/CDA.cpp
+++ src/CDA.cpp
@@ -4,6 +4,8 @@
 namespace vm {
 namespace cda {
 
+#if (BYTE_ORDER != LITTLE_ENDIAN)
+// Big Endian -> RGB
 const dword_t CDA::pallete[] = {    
     0x000000, // Black
     0x0000CD, // Dark Blue
@@ -23,6 +25,28 @@ const dword_t CDA::pallete[] = {
     0xFFFF00, // Yellow
     0xFFFFFF, // White
 };
+#else
+// Little Endian -> BGR
+const dword_t CDA::pallete[] = {    
+    0x000000, // Black
+    0xCD0000, // Dark Blue
+    0x00CD00, // Dark Green
+    0xCDCD00, // Dark Cyan
+    0x0000CD, // Dark Red
+    0xCD00CD, // Dark Magenta
+    0x0055AA, // Brown
+    0xCDCDCD, // Light Gray
+    // Bright colors
+    0x555555, // Dark Grey
+    0xFF0000, // Blue
+    0x00FF00, // Green
+    0xFFFF00, // Cyan
+    0x0000FF, // Red
+    0xFF00FF, // Magenta
+    0x00FFFF, // Yellow
+    0xFFFFFF, // White
+};
+#endif
 
 } // End of namespace cda
 } // End of namespace vm
