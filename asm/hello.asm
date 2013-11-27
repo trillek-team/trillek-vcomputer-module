15846b5e03c71dbf22cedda1873a35ea3a65a553
Luis Panadero Guardeño <luis.panadero@gmail.com>
2013-11-27 18:35:21 +0100

    Updated to use String literals and .include. Converted hwm.asm and hello.asm to latin1 encoding as CDA not handles multi-byte encodings

--- asm/hello.asm
+++ asm/hello.asm
@@ -14,31 +14,10 @@
 
         SLEEP                   ; End of program 
 
-; *****************************************************************************
-; Print function
-; %r0 <- Ptr to Null terminated string (ASCIIz)
-; %r1 <- Ptr to text screen buffer
-; %r2 <- Color atributte
-; return nothing
-print:
-        LLS %r2, %r2, 8
 
-print_loop:
-        LOAD.B %r3, %r0         ; Loads in %r3 a character
-        IFEQ %r3, 0             ; If is NULL then ends
-          RJMP print_end
-
-        OR %r3, %r3, %r2        ; Add color attribute
-        STORE.W %r1, %r3
-
-        ADD %r1, %r1, 2         ; Increment pointers and do the loop
-        ADD %r0, %r0, 1
-        RJMP print_loop
-
-print_end:
-        
-        RET
+        .include "lib.inc"
+  
 
 ;******************************************************************************
 ; Data
-string: .DB 'H', 'e', 'l', 'l', 'o', 0x20, 'w', 'o', 'r', 'l', 'd', '!', 0 ; ASCIIz string
+string: .DB "Hello world!", 0   ; ASCIIz string
