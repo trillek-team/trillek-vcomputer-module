9e59ea69dd84641c877387b9e571ed32aa3915da
Luis Panadero Guardeño <luis.panadero@gmail.com>
2013-12-14 22:10:59 +0100

    Basic WebGL setup. Need to rewrite wrapper/ToRGBATexture JS replazament that writes in a ImageData object

--- emscripten/vm_wrap.cpp
+++ emscripten/vm_wrap.cpp
@@ -21,6 +21,10 @@ extern "C" {
   }
 }
 
+bool AddGKey_(vm::VirtualComputer& arr, unsigned slot, vm::keyboard::GKeyboard& d ) {
+  return arr.AddDevice(slot, d);
+}
+
 bool AddCDA_(vm::VirtualComputer& arr, unsigned slot, vm::cda::CDA& d ) {
   return arr.AddDevice(slot, d);
 }
@@ -47,6 +51,7 @@ EMSCRIPTEN_BINDINGS(rc3200_vm) {
       .constructor<int>()
       .function("Reset",      &vm::VirtualComputer::Reset)
       .function("WriteROM",   &vm::VirtualComputer::WriteROM, allow_raw_pointers())
+      .function("AddKeyboard",&AddGKey_)
       .function("AddCDA",     &AddCDA_)
       .function("RemoveDevice",  &vm::VirtualComputer::RemoveDevice)
       .function("CPUState",   &vm::VirtualComputer::CPUState)
@@ -64,7 +69,26 @@ EMSCRIPTEN_BINDINGS(rc3200_vm) {
       .function("VSync",      &vm::cda::CDA::VSync)
       .function("ToRGBATexture",  &WriteTexture_, allow_raw_pointers())
       ;
+    
+    class_<vm::keyboard::GKeyboard>("GKeyboard")
+      .constructor<int, int>()
+      .function("PushKeyEvent",  &vm::keyboard::GKeyboard::PushKeyEvent)
+      ;
 
+    enum_<vm::keyboard::SCANCODES>("SCANCODES")
+      .value("BACKSPACE", vm::keyboard::BACKSPACE)
+      .value("RETURN", vm::keyboard::RETURN)
+      .value("INSERT", vm::keyboard::INSERT)
+      .value("DELETE", vm::keyboard::DELETE)
+      .value("ESC", vm::keyboard::ESC)
+      .value("ARROW_UP", vm::keyboard::ARROW_UP)
+      .value("ARROW_DOWN", vm::keyboard::ARROW_DOWN)
+      .value("ARROW_LEFT", vm::keyboard::ARROW_LEFT)
+      .value("ARROW_RIGHT", vm::keyboard::ARROW_RIGHT)
+      .value("SHIFT", vm::keyboard::SHIFT)
+      .value("CONTROL", vm::keyboard::CONTROL)
+      ;
+      
 }
 
 
