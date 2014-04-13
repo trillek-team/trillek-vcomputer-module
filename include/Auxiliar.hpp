71da819eb1bdbb0f1b353621a8d8f0b1fcc7ff71
Luis Panadero Guardeño <luis.panadero@gmail.com>
2014-04-13 11:22:21 +0200

    Test new ident and code style rules

--- include/Auxiliar.hpp
+++ include/Auxiliar.hpp
@@ -1,23 +1,26 @@
 #pragma once
-/**
- * Trillek Virtual Computer - Auxiliar.hpp
- * Some auciliar functions and methods
+/*!
+ * \brief       Virtual Computer Auxiliar functions
+ * \file        Auxiliar.hpp
+ * \copyright   The MIT License (MIT)
+ *
+ * Some auxiliar functions and methods
  */
 
 #include "Types.hpp"
 #include <string>
 
 namespace vm {
-  namespace aux {
+    namespace aux {
 
-    /**
-     * Load a raw binary file as ROM
-     * @param filename Binary file with the ROM
-     * @param rom buffer were to write it
-     * @return Read size or negative value if fails
-     */
-    int LoadROM (const std::string& filename, byte_t* rom);
+        /*!
+         * Load a raw binary file as ROM
+         * \param[in] filename Binary file with the ROM
+         * \param[out] rom buffer were to write it
+         * \return Read size or negative value if fails
+         */
+        int LoadROM (const std::string& filename, byte_t* rom);
 
-  } // end of namespace aux
+    } // end of namespace aux
 } // end of namespace vm
 
