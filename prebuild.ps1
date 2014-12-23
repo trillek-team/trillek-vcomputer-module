ac077ffddf43e23dbebaa1677e7b01c6caf20f03
Luis Panadero Guardeño <luis.panadero@gmail.com>
2014-12-23 23:20:09 +0100

    Using NuGet to grab libs

--- prebuild.ps1
+++ prebuild.ps1
@@ -1,37 +1,24 @@
 # PowerShell script to install all dependencies
+#
+# Use choco & nuget to grab glew and glfw
+choco install NuGet.CommandLine
 
 new-item lib -type directory
 new-item lib\x86 -type directory
 new-item lib\x86\Debug -type directory
 new-item lib\include -type directory
-new-item build -type directory
+new-item lib\include\glm -type directory
+new-item build -type director
 
-# Get GLEW
-Start-FileDownload 'http://softlayer-ams.dl.sourceforge.net/project/glew/glew/1.10.0/glew-1.10.0-win32.zip'
-7z -y x glew-1.10.0-win32.zip
-move-item -path glew-1.10.0\lib\Release\Win32\*.lib -destination lib/x86/Debug
-move-item -Path glew-1.10.0\include\* -destination lib/include
-
-# Get GLFW
-Start-FileDownload 'http://skylink.dl.sourceforge.net/project/glfw/glfw/3.0.4/glfw-3.0.4.bin.WIN32.zip'
-7z -y x glfw-3.0.4.bin.WIN32.zip
-move-item -path glfw-3.0.4.bin.WIN32\lib-msvc120\*.lib -destination lib/x86/Debug
-move-item -path glfw-3.0.4.bin.WIN32\include\* -destination lib/include
-
-# Get GLM
-Start-FileDownload 'http://heanet.dl.sourceforge.net/project/ogl-math/glm-0.9.5.3/glm-0.9.5.3.7z'
-7z -y x glm-0.9.5.3.7z
-move-item -path glm\* -destination lib/include
-
-# Get and build GTest
-Start-FileDownload 'http://googletest.googlecode.com/files/gtest-1.7.0.zip'
-7z -y x gtest-1.7.0.zip
 pushd
-Set-Location gtest-1.7.0
-cmake -G "Visual Studio 12" -D CMAKE_BUILD_TYPE:String=Release .
-msbuild gtest.sln /p:Configuration=Release /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+# glew
+# C:\glew.1.9.0.1\build\native
+nuget install glew 
+Copy-Item -path c:\glew.1.9.0.1\include\* -destination lib/include
+
+nuget install GLFW
+nuget install glm
+#nuget install openal.redist
+nuget install gtest
+
 popd
-move-item -path gtest-1.7.0\include\* -destination lib/include
-move-item -path gtest-1.7.0\Release\*.lib -destination lib/x86/Release
-move-item -path gtest-1.7.0\Release\*.dll -destination lib/x86/Release
-Get-ChildItem lib/x86/Release/*
