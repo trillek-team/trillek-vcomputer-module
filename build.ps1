85651c8d4d9ec70a37461aa589744edcb1c33197
Luis Panadero Guardeño <luis.panadero@gmail.com>
2014-12-23 23:19:23 +0100

    Some corrections...

--- build.ps1
+++ build.ps1
@@ -1,6 +1,4 @@
-cmake -G "Visual Studio 12" -D CMAKE_BUILD_TYPE:String=Release .
-msbuild Trillek_Client.vcxproj /p:Configuration=Release /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-cd tests
-cmake -G "Visual Studio 12" -D CMAKE_BUILD_TYPE:String=Release .
-msbuild TCCTests.vcxproj /p:Configuration=Release /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+# TODO set GTEST_ROOT to were is located gtest
+cmake -G "Visual Studio 12" -D CMAKE_BUILD_TYPE:String=Release -DBUILD_STATIC_VCOMPUTER=True .
+msbuild VCOMPUTER.sln /p:Configuration=Release /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
