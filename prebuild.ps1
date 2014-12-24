557f731044a99b6fd896ce3b21407d9be737662f
Luis Panadero Guardeño <luis.panadero@gmail.com>
2014-12-24 10:42:58 +0100

    Gtest download directly
    NuGet install of GTests lacks of cmake files so we can't use it

--- prebuild.ps1
+++ prebuild.ps1
@@ -1,7 +1,6 @@
 # PowerShell script to install all dependencies
 #
 # Use choco & nuget to grab glew and glfw
-choco install NuGet.CommandLine
 
 new-item lib -type directory
 new-item lib\x86 -type directory
@@ -11,15 +10,10 @@ new-item lib\include\glm -type directory
 new-item build -type director
 
 pushd
-# glew
-# C:\glew.1.9.0.1\build\native
-#nuget install glew 
-#Copy-Item -path c:\glew.1.9.0.1\include\* -destination lib/include
 
-#nuget install GLFW
-#nuget install glm
-#nuget install openal.redist
-nuget install gtest
-$env:GTEST_ROOT = "C:\gtest.1.7.0.1\"
+Start-FileDownload 'https://googletest.googlecode.com/files/gtest-1.7.0.zip'
+7z -y x gtest-1.7.0.zip
+
+$env:GTEST_ROOT = $PWD.Path + "\gtest-1.7.0\"
 
 popd
