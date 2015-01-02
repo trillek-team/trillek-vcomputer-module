fbdcc36c19a4c9a85f4332e94ffd630c946604e0
Luis Panadero Guardeño <luis.panadero@gmail.com>
2015-01-02 17:34:26 +0100

    Added an overload of LoadROM
    Accepts a istream as source of data.
    LoadROM (filename, *rom) now uses it internally.

--- src/auxiliar.cpp
+++ src/auxiliar.cpp
@@ -27,24 +27,39 @@ int LoadROM (const std::string& filename, Byte* rom) {
         if ( !f.is_open() ) {
             return -1;
         }
+        count = LoadROM(f, rom);
 
-        size_t size;
-
-        auto begin = f.tellg();
-        f.seekg (0, std::ios::end);
-        auto end = f.tellg();
-        f.seekg (0, std::ios::beg);
-
-        size = end - begin;
-        size = size > (MAX_ROM_SIZE) ? (MAX_ROM_SIZE) : size;
-
-        f.read(reinterpret_cast<char*>(rom), size);
-        count = size;
     }
     catch (...) {
         count = -1;
     }
 
+    return count;
+} // LoadROM
+
+
+int LoadROM (std::istream& stream, Byte* rom) {
+    assert(rom != nullptr);
+    
+    int count;
+    try {
+        size_t size;
+        
+        auto begin = stream.tellg();
+        stream.seekg (0, std::ios::end);
+        auto end = stream.tellg();
+        stream.seekg (0, std::ios::beg);
+        
+        size = end - begin;
+        size = size > (MAX_ROM_SIZE) ? (MAX_ROM_SIZE) : size;
+        
+        stream.read(reinterpret_cast<char*>(rom), size);
+        count = size;
+    }
+    catch (...) {
+        count = -1;
+    }
+    
     return count;
 } // LoadROM
 
