d656f3d20a3af371808e46fab2e1d4fa2c029198
Luis Panadero Guardeño <luis.panadero@gmail.com>
2015-01-29 23:23:40 +0100

    :green_heart: Should fix error with travis
    And should allow to appveyour to find correctly gtest

--- cmake/FindGTEST.cmake
+++ cmake/FindGTEST.cmake
@@ -14,11 +14,17 @@ ENDIF (GTEST_INCLUDE_DIR)
 
 FIND_PATH(	GTEST_INCLUDE_DIR
 			NAMES gtest/gtest.h
+      HINTS
+        $ENV{GTEST_ROOT}/include
+        ${GTEST_ROOT}/include
 			PATHS /usr/install
 			PATH_SUFFIXES gtest-1.7.0/include gtest-1.7.0)
 
 FIND_LIBRARY(	GTEST_LIBRARY
 				NAMES gtest
+        HINTS
+          $ENV{GTEST_ROOT}
+          ${GTEST_ROOT}
 				PATHS /usr/lib /usr/lib64 /usr/lib32 /usr/local/lib
 				PATH_SUFFIXES x86 x64 amd64 lib64)
 
