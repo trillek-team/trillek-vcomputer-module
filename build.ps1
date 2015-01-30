3fcf38bbf0fbfc3fd72a91fdba9660903c256ecf
Luis Panadero Guardeño <luis.panadero@gmail.com>
2015-01-30 17:02:09 +0100

    Test appveyour building 3

--- build.ps1
+++ build.ps1
@@ -1,10 +1,6 @@
 cmake -version
 
-cmake -G "Visual Studio 12"  .
+cmake -G "Visual Studio 12" -DBUILD_TOOLS_VCOMPUTER=False .
 
 msbuild VCOMPUTER.sln /p:Configuration=Debug /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-
-cd bin
-cd Debug
-unit_test.exe
 
