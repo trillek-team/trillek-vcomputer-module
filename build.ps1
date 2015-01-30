a3310eaa1d85300a2e0bada9e699f03275ab8c09
Luis Panadero Guardeño <luis.panadero@gmail.com>
2015-01-30 16:51:07 +0100

    Test appveyour building 2
    Only corrects the name of the unit test file

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
-cmake
+cmake -version
 
-cmake -G "Visual Studio 12"  -DBUILD_TOOLS_VCOMPUTER=False .
+cmake -G "Visual Studio 12"  .
 
 msbuild VCOMPUTER.sln /p:Configuration=Debug /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
 cd bin
 cd Debug
-unit_test
+unit_test.exe
 
