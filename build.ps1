919d24f0ad0cb94ebb8a78854f8093fdbbcbfe55
Luis Panadero Guardeño <luis.panadero@gmail.com>
2015-01-30 16:39:54 +0100

    Test appveyour building
    Try to download libs zip file from my hosting

--- build.ps1
+++ build.ps1
@@ -1,7 +1,10 @@
-$env:GTEST_ROOT = $PWD.Path + "\gtest-1.7.0\"
-cmake -G "Visual Studio 12" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TOOLS_VCOMPUTER=False .
+cmake
+
+cmake -G "Visual Studio 12"  -DBUILD_TOOLS_VCOMPUTER=False .
 
 msbuild VCOMPUTER.sln /p:Configuration=Debug /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
-ctest -VV
+cd bin
+cd Debug
+unit_test
 
