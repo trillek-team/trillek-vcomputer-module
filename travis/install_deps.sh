07f1d843b28b4e43e7675c5ad24150cfc1e6cbf1
Luis Panadero Guardeño <luis.panadero@gmail.com>
2015-09-26 15:12:38 +0200

    Adding clang to travis matrix

--- travis/install_deps.sh
+++ travis/install_deps.sh
@@ -2,29 +2,29 @@
 
 set -e
 
-if [ ! -d "$HOME/glfw-lib/lib" ]; then
+if [ ! -d "$HOME/lib/$COMPILER/glfw-lib/lib" ]; then
     # No ppa for GLFW3 at this time. Compiling by hand!
     git clone --depth=100 https://github.com/glfw/glfw.git
     pushd glfw
-    CMAKE_CXX_FLAGS=-fPIC CMAKE_C_FLAGS=-fPIC cmake -DCMAKE_INSTALL_PREFIX=$HOME/glfw-lib -DBUILD_SHARED_LIBS:bool=true . && make && make install
+    CMAKE_CXX_FLAGS=-fPIC CMAKE_C_FLAGS=-fPIC cmake -DCMAKE_INSTALL_PREFIX=$HOME/lib/$COMPILER/glfw-lib -DBUILD_SHARED_LIBS:bool=true . && make && make install
     popd
 else
     echo "GLFW - Using cached directory";
 fi
 
-if [ ! -d "$HOME/glm/glm-0.9.5.3" ]; then
+if [ ! -d "$HOME/lib/$COMPILER/glm/glm-0.9.5.3" ]; then
     #Download GLM 9.5 from github
     wget https://github.com/g-truc/glm/archive/0.9.5.3.zip
-    unzip 0.9.5.3.zip -d $HOME/glm
+    unzip 0.9.5.3.zip -d $HOME/lib/$COMPILER/glm
 else
     echo "GLM - Using cached directory";
 fi
 
-if [ ! -d "$HOME/gtest-1.7.0/include" ]; then
+if [ ! -d "$HOME/lib/$COMPILER/gtest-1.7.0/include" ]; then
     # Download GTest
     wget http://googletest.googlecode.com/files/gtest-1.7.0.zip
-    unzip gtest-1.7.0.zip -d $HOME
-    cd $HOME/gtest-1.7.0
+    unzip gtest-1.7.0.zip -d $HOME/lib/$COMPILER
+    cd $HOME/lib/$COMPILER/gtest-1.7.0
     cmake . && make
 else
     echo "Google Test - Using cached directory";
